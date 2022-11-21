b202f0d321a269586c4f56fd52e8110d9428d6bc
v.berezovskyi <v.berezovskyi@globallogic.com>
2022-11-21 12:10:58 +0200

    first commit

---
+++ src/main/java/com/kingsm01n/kpicursova/KpiCursovaApplication.java
@@ -0,0 +1,13 @@
+package com.kingsm01n.kpicursova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiCursovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpiCursovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingsm01n/kpicursova/KpiCursovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingsm01n.kpicursova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiCursovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
